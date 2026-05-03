35230c05d7f1f537099b2ce297030cf55dc832be
voxsledderman <bartoszswora66@gmail.com>
2026-05-03 21:17:15 +0200

    feat: bishop moves

--- src/main/java/org/voxsledderman/logic/Bishop.java
+++ src/main/java/org/voxsledderman/logic/Bishop.java
@@ -2,7 +2,7 @@ package org.voxsledderman.logic;
 
 import org.voxsledderman.enums.ChessColor;
 
-import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,6 +13,33 @@ public class Bishop extends Piece{
 
     @Override
     public List<Move> getMoves(Collection<Piece> pieces) {
-        return List.of();
+        List<Move> moves = new ArrayList<>();
+        Move[] directions = new Move[]{
+                Move.from(1,1), Move.from(1,-1), Move.from(-1,-1), Move.from(-1,1)
+        };
+        for(Move dir : directions){
+            moves.addAll(getDirectionMoves(dir, pieces));
+        }
+
+        return moves;
+    }
+    private List<Move> getDirectionMoves(Move dir, Collection<Piece> pieces){
+        List<Move> dirMoves = new ArrayList<>();
+        for(int i = 1; i < 8; i++){
+            Move move = Move.from(getRow() + (i * dir.row()), getCol() + (i * dir.col()));
+
+            if(isMoveInBoard(move)){
+                Piece pieceAt = getPieceAt(move.row(), move.col(), pieces);
+                if(pieceAt == null){
+                    dirMoves.add(move);
+                } else {
+                    if(isDifferentColor(pieceAt)){
+                        dirMoves.add(move);
+                    }
+                    break;
+                }
+            } else break;
+        }
+        return dirMoves;
     }
 }
